362f19464c379f27ae68e2a7316f96c6cb25a9f9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-03-09 22:24:46 +0700

    #

--- show-account-KIA.js
+++ show-account-KIA.js
@@ -11,53 +11,53 @@
 // ==/UserScript==
 
 window.addEventListener('DOMContentLoaded', function () {
-   'use strict';
+    'use strict';
 
-   function findUser(id) {
-       let token = document.getElementsByName("_xfToken")[0].value;
-       let queryUrl = "https://voz.vn/index.php?members/find&q={username}&_xfRequestUri={requestUri}&_xfWithData=1&_xfToken={token}&_xfResponseType=json";
-       let username = document.querySelector(".message-userDetails a.username[data-user-id='" + id + "']").innerText;
-       queryUrl = queryUrl.replace("{username}", encodeURIComponent(username))
-           .replace("{requestUri}", document.location.pathname)
-           .replace("{token}", token);
+    function findUser(id) {
+        let token = document.getElementsByName("_xfToken")[0].value;
+        let queryUrl = "https://voz.vn/index.php?members/find&q={username}&_xfRequestUri={requestUri}&_xfWithData=1&_xfToken={token}&_xfResponseType=json";
+        let username = document.querySelector(".message-userDetails a.username[data-user-id='" + id + "']").innerText;
+        queryUrl = queryUrl.replace("{username}", encodeURIComponent(username))
+            .replace("{requestUri}", document.location.pathname)
+            .replace("{token}", token);
 
-       let httpRequest = new XMLHttpRequest();
-       httpRequest.onreadystatechange = function() {
-           if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-               let isKIA = !JSON.parse(httpRequest.responseText).results.some(function (r) { return r.id === username; });
-               gotResult(id, isKIA);
-           }
-       }
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+                let isKIA = !JSON.parse(httpRequest.responseText).results.some(function (r) { return r.id === username; });
+                gotResult(id, isKIA);
+            }
+        }
 
-       httpRequest.open("GET", queryUrl);
-       httpRequest.send();
-   }
+        httpRequest.open("GET", queryUrl);
+        httpRequest.send();
+    }
 
-   function gotResult(id, isKIA) {
-       if (isKIA) {
-           let els = document.querySelectorAll(".message-userDetails a.username[data-user-id='" + id + "']");
-           els.forEach(el => {
-               el.parentElement.parentElement.querySelector("[itemprop=jobTitle]").innerHTML = "<strong styke='red;'>Nơi đảo xa</strong>";
-           });
-       }
-   }
+    function gotResult(id, isKIA) {
+        if (isKIA) {
+            let els = document.querySelectorAll(".message-userDetails a.username[data-user-id='" + id + "']");
+            els.forEach(el => {
+                el.parentElement.parentElement.querySelector("[itemprop=jobTitle]").innerHTML = "<strong style='color: #DC143C'>Nơi đảo xa</strong>";
+            });
+        }
+    }
 
-   let handledIds = [];
-   function inoHandler(entries, observer) {
-       entries.forEach(entry => {
-           let id = entry.target.getAttribute("data-user-id");
-           if (handledIds.includes(id)) {
-               observer.unobserve(entry.target);
-           } else if (entry.isIntersecting) {
-               findUser(id);
-               handledIds.push(id);
-           }
-       });
-   }
-   let observer = new IntersectionObserver(inoHandler);
+    let handledIds = [];
+    function inoHandler(entries, observer) {
+        entries.forEach(entry => {
+            let id = entry.target.getAttribute("data-user-id");
+            if (handledIds.includes(id)) {
+                observer.unobserve(entry.target);
+            } else if (entry.isIntersecting) {
+                findUser(id);
+                handledIds.push(id);
+            }
+        });
+    }
+    let observer = new IntersectionObserver(inoHandler);
 
-   let els = document.querySelectorAll(".message-userDetails a.username");
-   els.forEach(el => observer.observe(el));
+    let els = document.querySelectorAll(".message-userDetails a.username");
+    els.forEach(el => observer.observe(el));
 
 
 });
