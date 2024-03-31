dfb65b80a62dae1364c92a8a8d042ad6cad47d77
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-03-31 19:50:57 +0700

    #

--- paimon.moe.js
+++ paimon.moe.js
@@ -1,30 +1,26 @@
 // ==UserScript==
 // @version      1.0.0
 // @name         Paimon Moe
-// @description  Test
+// @description  Show not achieved first
 
 // @namespace https://github.com/nguyenhuyenag
 // @icon https://paimon.moe/favicon.png
 // @author Fzen
-// @homepage https://github.com/nguyenhuyenag/youtube-more-speeds
-// @supportURL https://github.com/ssssssander/youtube-more-speeds/issues
+// @homepage https://github.com/nguyenhuyenag/monkey-script
+// @supportURL https://github.com/nguyenhuyenag/monkey-script/issues
 // @match *://*.paimon.moe/*
 // @license MIT
 // ==/UserScript==
 
 (function () {
     "use strict";
+
     // Select the first checkbox input element on the page
     const firstCheckbox = document.querySelector('input[type="checkbox"]:first-of-type');
 
-    // Check if the checkbox element exists
     if (firstCheckbox) {
-        // Select the parent element of the checkbox
         const parentElement = firstCheckbox.parentNode;
-
-        // Check if the parent element exists
         if (parentElement) {
-            // Trigger a click event on the parent element
             parentElement.click();
         } else {
             console.log("Parent element not found.");
