47fcc21b7f0522e10e0a7b74b04656957bb9f0b9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-09-25 07:55:26 +0700

    #

---
+++ youtube-visited-links.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          Youtube visited links
+// @description
+// @namespace     Violentmonkey Scripts
+// @match         https://www.youtube.com/*
+// @grant         GM_addStyle
+// @version       0.0.1
+// @author        -
+// @license       MIT
+// @run-at        document-idle
+// ==/UserScript==
+
+
+(function main(){
+  const css=`h3>a[id*="video-title"]:visited>yt-formatted-string {
+  color: red !important;
+  font-weight:200 !important;
+}
+a:visited>h3>span#video-title {
+  color: red !important;
+  font-weight:200 !important;
+}`
+  let csss=GM_addStyle(css).then(el=>console.log('injected ',el));
+})()
