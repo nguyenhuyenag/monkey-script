1ac39d68889804427559169e0ebd978529e112e1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-10-05 12:47:07 +0700

    #

--- paimon.moe.js
+++ paimon.moe.js
@@ -1,35 +1,38 @@
 // ==UserScript==
 // @version      1.0.0
-// @name         Paimon Moe
-// @description  Show not achieved first
-
-// @namespace https://github.com/nguyenhuyenag
-// @icon https://paimon.moe/favicon.png
-// @author nguyenhuyenag
-// @homepage https://github.com/nguyenhuyenag/monkey-script
-// @supportURL https://github.com/nguyenhuyenag/monkey-script/issues
-// @match *://*.paimon.moe/*
-// @license MIT
+// @name         Genshin Achievement
+// @description  Genshin Achievement
+// @namespace    https://github.com/nguyenhuyenag
+// @icon         https://paimon.moe/favicon.png
+// @author       nguyenhuyenag
+// @homepage     https://github.com/nguyenhuyenag/monkey-script
+// @supportURL   https://github.com/nguyenhuyenag/monkey-script/issues
+// @match        *://*.paimon.moe/*
+// @license      MIT
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    function checkboxHandler() {
-        const showNotAchieved = document.querySelector('input[type="checkbox"]:first-of-type');
-        if (!showNotAchieved) {
-            console.log("Checkbox element not found!");
-            return;
-        }
-        if (!showNotAchieved.checked) {
-            showNotAchieved.click();
-        } else {
-            console.log("Checkbox already checked!");
-        }
-    }
-    
-    checkboxHandler();
-    
-    document.addEventListener('transitionend', checkboxHandler);
-    window.addEventListener('popstate', checkboxHandler);
+    const hiddenTexts = [
+        'Hoàn thành thí nghiệm quang học lúc trước.',
+        'Sử dụng năng lực của Bạch Tuộc Bóng Tròn Dị Sắc.'
+    ];
+
+    $(function () {
+        let items = $('.bg-item');  // Select all elements with the class 'bg-item'
+        console.log("xxxxxxxxxxxxxxxxxxxx:", items.length);
+
+        items.each(function () {
+            let descriptionElement = $(this).find('p.text-gray-400.svelte-lpdzps');  // Find the specific <p> inside the current item
+            if (descriptionElement.length) {
+                let description = descriptionElement.text().trim();
+                if (hiddenTexts.includes(description)) {
+                    console.log("yyyyyyyyyyyyyyyyyyyyy:", this); // Log the item being hidden
+                    $(this).hide(); // Hide the current item
+                }
+            }
+        });
+    });
 })();
